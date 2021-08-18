289c3552de86e0090c9f3f90cdf543ac731ad91d
Doramon <dora@abc.com>
2021-08-18 10:17:35 +0700

    update api

--- api/connected.php
+++ api/connected.php
@@ -3,4 +3,4 @@
     error_reporting(0);
     error_reporting(E_ERROR | E_PARSE);
     header("content-type:text/javascript;charset=utf-8");
-    $link = mysqli_connect('localhost', 'taradsod', 'Abc12345', "taradsod1");
+    $link = mysqli_connect('localhost', 'taradsod2', 'Sc0830392181', 'taratsod a');
